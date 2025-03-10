6070e41ae9dde3f01088c3aea889097d33b16f28
Валентин Сергийович Николенко <VSNikolenko22@codingburgas.bg>
2025-03-10 15:46:20 +0200

    Create files and folders

---
+++ login/loginpage.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+	cout << "Sign in" << endl;
+	string username;
+	string password;
+	cout << "Enter the username";
+	cin >> username;
+	cout << "Enter the password";
+	cin >> password;
+
+	}
+
