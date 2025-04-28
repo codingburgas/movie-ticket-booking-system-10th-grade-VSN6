e4ba704e3a6912d0ef3b29b01c6b382d971d31c4
VSNikolenko22 <VSNikolenko22@codingburgas.bg>
2025-04-28 15:46:51 +0300

    Menu Changes

--- main.cpp
+++ main.cpp
@@ -11,14 +11,16 @@ int main() {
     int ans;
     cout << "WELCOME" << endl << "Before you book a place, you need to answer a few questions." << endl << "  "<< endl;
     while (true) {
-        cout << "-----------Menu----------" << endl;
-        cout << "1. Add cinema"<< endl;
-        cout << "2. Show cinemas"<< endl;
-        cout << "3. Add movie"<< endl;
-        cout << "4. Show movies"<< endl;
-        cout << "5. Add show"<< endl;
-        cout << "6. Book a place" << endl;
-        cout << "0. Exit" << endl;
+
+        cout << "    _____________Menu_____________" << endl;
+        cout << "  || 1. Add cinema                 ||"<< endl;
+        cout << "  || 2. Show cinemas               ||"<< endl;
+        cout << "  || 3. Add movie                  ||"<< endl;
+        cout << "  || 4. Show movies                ||"<< endl;
+        cout << "  || 5. Add show                   ||"<< endl;
+        cout << "  || 6. Book a place               ||" << endl;
+        cout << "  || 0. Exit                       ||" << endl;
+        cout << "     _____________________________   " << endl;
         cin >> ans;
         if (ans == 0) {
             exit(0);
